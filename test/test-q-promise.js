0014fb33cc9f91fcc27d564a6829b0b185e8c48c
Andras <andras@kinvey.com>
2017-04-09 15:02:21 -0400

    more unit tests

--- test/test-q-promise.js
+++ test/test-q-promise.js
@@ -12,6 +12,11 @@ describe ('q-promise', function(){
         done();
     })
 
+    afterEach (function(done) {
+        P.onResolve(false);
+        done();
+    })
+
     describe ('constructor', function(){
         it ('executor should be optional', function(done) {
             var p = new P();
@@ -42,10 +47,26 @@ describe ('q-promise', function(){
             qassert.equal(p.state, 'n', "wanted rejected");
             done();
         })
+
+        it ('should call __resolve with return fulfillment value', function(done) {
+            var called;
+            P.onResolve(function(v, p, s) { called = [v, p, s] })
+            var p = new P(function(resolve, reject) { resolve(123) });
+            qassert.deepEqual(called, [123, p, 'y']);
+            done();
+        })
+
+        it ('should call __resolve with rejection cause', function(done) {
+            var called;
+            P.onResolve(function(v, p, s) { called = [v, p, s] })
+            var p = new P(function(resolve, reject) { reject(123) });
+            qassert.deepEqual(called, [123, p, 'n']);
+            done();
+        })
     })
 
     describe ('resolve', function(){
-        it ('should return a resolved promise', function(done) {
+        it ('should return a fulfilled promise', function(done) {
             var p = P.resolve(123);
             qassert(p instanceof P);
             qassert.equal(p.state, 'y');
@@ -53,17 +74,21 @@ describe ('q-promise', function(){
             done();
         })
 
-        // TODO: this might not be spec, but seems useful
-        it ('should resolve a function value', function(done) {
-            var p = P.resolve(function(){ return 123 });
-            process.nextTick(function(){
-                qassert.equal(p.state, 'y');
-                qassert.equal(p.value, 123);
-                done();
-            })
+        it ('should call __resolve on a value', function(done) {
+            var called;
+            P.onResolve(function(v, p, s){ called = [v, p, s] });
+            var p = P.resolve(1);
+            qassert.deepEqual(called, [1, p, 'y']);
+            done();
         })
 
-        // TODO: 'should invoke __resolve'
+        it ('should call __resolve on a function result', function(done) {
+            var called;
+            P.onResolve(function(v, p, s){ called = [v, p, s] });
+            var p = P.resolve(function(){ return 123 });
+            qassert.deepEqual(called, [123, p, 'y']);
+            done();
+        })
     })
 
     describe ('reject', function(){
@@ -75,29 +100,64 @@ describe ('q-promise', function(){
             done();
         })
 
-        // TODO: 'should invoke __resolve'
+        it ('should call __resolve on a value', function(done) {
+            var called;
+            P.onResolve(function(v, p, s){ called = [v, p, s] });
+            var p = P.reject(123);
+            qassert.deepEqual(called, [123, p, 'n']);
+            done();
+        })
+        it ('should call __resolve on a function result', function(done) {
+            var called;
+            P.onResolve(function(v, p, s){ called = [v, p, s] });
+            var p = P.reject(function(){ return 123 });
+            qassert.deepEqual(called, [123, p, 'n']);
+            done();
+        })
     })
 
     describe ('then', function(){
     })
 
     describe ('catch', function(){
+        it ('should call then', function(done) {
+            var called;
+            var handler = function(){};
+            p.then = function(resolve, reject) { called = [resolve, reject] };
+            p.catch(handler);
+            qassert.deepEqual(called, [undefined, handler]);
+            done();
+        })
     })
 
-    describe ('helpers', function(){
-        it ('should _resolve a promise', function(done) {
-            // TODO: should call __resolve
-            p._resolve(1234);
-            qassert.equal(p.state, 'y');
-            qassert.equal(p.value, 1234);
+    describe ('__resolve', function(){
+        it ('should resolve a value', function(done) {
             done();
         })
 
-        it ('should _reject a promise', function(done) {
-            // TODO: should call __resolve
-            p._reject(1234);
-            qassert.equal(p.state, 'n');
-            qassert.equal(p.value, 1234);
+        it ('should resolve an object', function(done) {
+            done();
+        })
+
+        it ('should resolve a promise', function(done) {
+            done();
+        })
+    })
+
+    describe ('helpers', function(){
+        it ('_resolve should call __resolve', function(done) {
+            var called;
+            P.onResolve(function(v, p, state) { called = [v, p, state] });
+            p._resolve(123);
+            qassert.deepEqual(called, [123, p, 'y']);
+            done();
+        })
+
+        it ('_reject should call __resolve', function(done) {
+            var called;
+            P.onResolve(function(v, p, state) { called = [v, p, state] });
+            p._reject(123);
+            qassert.deepEqual(called, [123, p, 'n']);
             done();
         })
     })
